a6b740446b8064ceadb0b5bb6dd8639d6fddf235
PrabhG999 <Prabhjeezizoo11@gmail.com>
2024-06-08 17:35:04 -0700

    studying and making commits started a book

---
+++ Arrays-Lists/Lists_1/Lists_1.py
@@ -0,0 +1,7 @@
+"""Example of lists in python which are also called as arrays"""
+a = [1, 'a', False]
+# Lists is dynamic in pyhton
+print(len(a))
+# above statemnet prints the list lenght in python.
+print(a[1])
+# above statement prints the value of 1st element in list a.
---
+++ Looping/Loopinh_1.py
@@ -0,0 +1,30 @@
+# Example of normal looping
+"""total = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    total += x
+    sum = total
+    print(total)
+print('sum is equal to', sum)"""
+
+# another case example of looping using range() function
+height = [5, 4, 7, 2, 3]
+
+weightedSum = 0
+"""
+for i in range(len(height)):
+    weightedSum += i * height[i]
+    wightedSum_1 = max(height)
+print('wightedSum', weightedSum)
+print('wightedSum_1', wightedSum_1)"""
+
+# example to use Enumeration() function
+for i, h in enumerate(height):
+    weightedSum += i * h
+    print('weighted sum per Itteration', weightedSum,
+          'Value of i per Itteration', i,
+          'Value of h per itterration', h)
+
+    """"
+    0+= 1*5 -> and so on (observe the above print statments)
+    """
+print("Total ", weightedSum)
