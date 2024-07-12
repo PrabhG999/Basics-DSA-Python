42778dd9a49246965914cdfe74c1cd617f85af5f
PrabhG999 <Prabhjeezizoo11@gmail.com>
2024-07-12 02:31:03 -0700

    Practiced Python OOPs

---
+++ Learning/July_12/import_oops_practice.py
@@ -0,0 +1,22 @@
+from OOPS_Practice import Cat
+
+my_cat = Cat('dollor',5,'black',3)
+
+my_cat.speak()
+print(my_cat.name)
+print(my_cat.age)
+print(my_cat.color)
+print(my_cat.weight)
+
+
+
+
+#Data types
+
+"""
+int integers
+float decimal 
+double big decimal 
+boolean true/false
+string 'Sonali' "Sonali"
+"""
